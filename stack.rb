aaf242802d70265c9a30556e0b8192c12517a124
Gabriel <gabriel@iplaces-MacBook-Pro.local>
2024-12-07 15:10:37 -0300

    move tests to its own files

--- stack.rb
+++ stack.rb
@@ -13,7 +13,7 @@ class Stack
   end
 
   def push(value)
-    new_node = Node.new(value, nil)
+    new_node = Node.new(value, current_node)
 
     self.current_node = new_node
   end
@@ -44,32 +44,5 @@ class Stack
 
   def clear
     self.current_node = nil
-  end
-end
-
-class TestStack < Minitest::Test
-  def setup
-    @stack = Stack.new
-  end
-
-  def test_that_push_works
-    @stack.clear
-    @stack.push(3)
-    @stack.push(2)
-    @stack.push(1)
-
-    assert_equal @stack.inspect, [1, 2, 3]
-  end
-
-  def test_that_pop_works
-    @stack.clear
-    @stack.push(3)
-    @stack.push(2)
-    @stack.push(1)
-
-    assert_equal @stack.pop, 1
-    assert_equal @stack.pop, 2
-    assert_equal @stack.pop, 3
-    assert_nil @stack.pop
   end
 end
